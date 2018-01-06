3c237fda2f6d1a7ac17ceceabddfb26e0d4e2105
George Marques <george@gmarqu.es>
2018-01-06 01:34:50 -0200

    Update copyright year

--- addons/vnen.tiled_importer/tiled_import_plugin.gd
+++ addons/vnen.tiled_importer/tiled_import_plugin.gd
@@ -1,6 +1,6 @@
 # The MIT License (MIT)
 #
-# Copyright (c) 2017 George Marques
+# Copyright (c) 2018 George Marques
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
